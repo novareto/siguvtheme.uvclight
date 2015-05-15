25699d2369ff43a7761614ae5517f6b549f4ee0b
goschtl <ck@novareto.de>
2015-05-15 10:48:48 +0200

    remove the example entry

--- src/siguvtheme/uvclight/entries.py
+++ src/siguvtheme/uvclight/entries.py
@@ -4,11 +4,3 @@ import uvclight
 from uvc.design.canvas.menus import INavigationMenu
 
 
-class SomeEntry(uvclight.MenuItem):
-    uvclight.title(u'Passwort ändern')
-    uvclight.auth.require('zope.Public')
-    uvclight.menu(INavigationMenu)
-
-    @property
-    def action(self):
-       return self.view.url(self.context)
